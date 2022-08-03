85c8768092843a8f1bbbf72cce60c5c52767a278
Alferavil88 <alferavil88@gmail.com>
2022-08-03 15:20:17 -0500

    feat(api/models model): add model Products

--- api/src/models/Products.js
+++ api/src/models/Products.js
@@ -1,1 +1,32 @@
-// index.
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('products', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false 
+    }
+  });
+};
