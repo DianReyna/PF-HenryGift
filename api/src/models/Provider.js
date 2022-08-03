b511fb82436f71f5640d57cc3feecb8d4f6fb230
Alferavil88 <alferavil88@gmail.com>
2022-08-03 15:25:30 -0500

    feat(api/models model): add model Provider

---
+++ api/src/models/Provider.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('providers', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    
+  });
+};
