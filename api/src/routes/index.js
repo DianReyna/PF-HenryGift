e36e5e94b2521398f79c5e6eb8ecfab4a2e585cd
DianReyna <reynaarmas@gmail.com>
2022-08-03 13:43:54 -0500

    refactor (api) add logic for routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const router = express.Router();
-const Boxes = require("../database");
+const { Router } = require("express");
+const router = Router();
+const AllBoxes = require("../routes/Boxes.routes.js");
 
-router.get("/boxes", (req, res) => {
-  res.json(Boxes);
-});
+router.use("/boxes", AllBoxes);
+
 module.exports = router;
