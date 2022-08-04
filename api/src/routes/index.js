0e79c011c0a4469790878a875e9779a77083f354
Alferavil88 <alferavil88@gmail.com>
2022-08-04 14:29:04 -0500

    feat/api categoty routes (method get and post) imported to index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const router = Router();
 const AllBoxes = require("../routes/Boxes.routes.js");
+const categotyRouter = require("./categoryRoutes")
 
 router.use("/boxes", AllBoxes);
-
+router.use("/categories",categotyRouter)
 module.exports = router;
