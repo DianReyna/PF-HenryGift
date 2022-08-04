3ea6c0a028aa563b4e06ae2c9c8a988f88baef10
DianReyna <reynaarmas@gmail.com>
2022-08-04 18:49:49 -0500

    add new endpoint post provider, change in api files: controllers, routes, service

--- api/src/controllers/providerControllers.js
+++ api/src/controllers/providerControllers.js
@@ -1,18 +1,36 @@
-const providerServices = require("../services/providerServices")
+const providerServices = require("../services/providerServices");
 
-const getAllProviders = async(req,res)=>{
-
+const getAllProviders = async (req, res) => {
   try {
-
-    const allProviders = await providerServices.getAllProviders()
-    res.send(allProviders)
-    
+    const allProviders = await providerServices.getAllProviders();
+    res.send(allProviders);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
+};
+const createNewProvider = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const newProvider = {
+      name: body.name,
+      phone: body.phone,
+      address: body.address,
+      email: body.email,
+    };
+    const createdProvider = await providerServices.createNewProvider(
+      newProvider
+    );
+    if (createdProvider) {
+      res.status(200).send("Provider Created!");
+    } else {
+      res.status(404).send("Error creating provider!");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-}
-
-module.exports={
-  getAllProviders
-}
+module.exports = {
+  getAllProviders,
+  createNewProvider,
+};
