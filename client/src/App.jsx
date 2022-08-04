f6da6f4da9bc4896db55fc30b534cde0e0dac70f
DaybelCR <dyi.cisnerosr@gmail.com>
2022-08-04 07:54:56 -0500

    feature carousel component (add Carousel.jsx)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./components/Home/Home";
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,22 +1,12 @@
-import React, {useEffect} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getBoxes } from '../../redux/actions/boxesActions';
-
+import React from 'react';
+import Carrusel from '../Carousel/Carousel';
 
 export default function Home() {
-
-  const dispatch = useDispatch();
-  const boxes = useSelector ((state)=> state.boxes)
-  
-
-  useEffect(()=>{
-    dispatch(getBoxes())
-  },[])
 
   return (
     <div>
       <h2>Soy Home</h2>
-      {boxes&&boxes.map((el,index)=><h1 key={index}>{el.name}</h1>)}
+      <Carrusel/>
     </div>
   )
 }
