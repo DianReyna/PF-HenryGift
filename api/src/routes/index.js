3788985538ff1715413938b01e37a75f4323993e
DianReyna <reynaarmas@gmail.com>
2022-08-04 14:14:14 -0500

    add endpoint products

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 const AllBoxes = require("../routes/Boxes.routes.js");
+const products = require("../routes/Products.routes.js");
 
 router.use("/boxes", AllBoxes);
+router.use("/products", products);
 
 module.exports = router;
