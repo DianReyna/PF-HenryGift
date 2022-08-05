2655d0ddcd8f4519053a7f705c120cee3d1aa579
Mayk0ll <vasquezloperamichael@gmail.com>
2022-08-05 18:18:53 -0500

    proceso de actualizacion de componentes

--- client/src/components/BoxDetail/BoxDetail.jsx
+++ client/src/components/BoxDetail/BoxDetail.jsx
@@ -3,6 +3,12 @@ import { Link, useParams } from 'react-router-dom';
 import { getBoxes } from "../../redux/actions/boxesActions";
 import { useSelector, useDispatch } from 'react-redux';
 import ProductCard from "../Products/ProductCard";
+import './BoxDetail.css';
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import StarBorderPurple500Icon from '@mui/icons-material/StarBorderPurple500';
 
 
 
@@ -20,71 +26,44 @@ useEffect(()=>{
 
 
   return (
+    <Container maxWidth="xl">
     <div>
-  
+          
       {
-        boxes[1]? 
-        <div>
-        <img src={boxes[1].image} alt='img not found'/>
-        <h4>Ranking: {boxes[1].ranking}</h4>
-        <h2>Box:{boxes[1].name}</h2>
-        <p>Detail:{boxes[1].detail}</p>
-        <h4>Person: {boxes[1].person}</h4>
-        <h4>Price: {boxes[1].price}</h4>
-        <h4>Expiration date: {boxes[1].expiration_date}</h4>
-        {boxes[1].product && boxes[1].product.map((product)=> 
-        (<ProductCard 
-        key={product.id}
-        id={product.id}
-        imagen={product.imagen} 
-        name={product.name} 
-        description={product.description}
-        location={product.location}/>))}
-        </div>
+        boxes[0]? 
+        <Grid container rowSpacing={4} className='containerDetail'>
+          <Grid item xs={6}>
+            <img src={boxes[0].imagen} alt='img not found' className="imagenDetail"/>
+          </Grid>
+          <Grid item xs={6}>
+            <div className="informationDetail">
+              <h1>Acerca de esta BOX:</h1>
+              
+              <h3>Box:{boxes[0].name}</h3>
+              <h3><PersonOutlineIcon color="secondary"/> Person: {boxes[0].person}</h3>
+              <h3><CardGiftcardIcon color="secondary"/> Contains {boxes[0].product.length} options</h3>
+              <h3><StarBorderPurple500Icon color="secondary"/> Ranking: {boxes[0].ranking}</h3>
+              <h1>Price: {boxes[0].price}</h1>
+              <h3>Expiration date: 30 days</h3>
+            </div>
+          </Grid>
+          <h3>Detail:{boxes[0].detail}</h3>
+          
+          {boxes[0].product && boxes[0].product.map((product)=> 
+          (<ProductCard 
+          key={product.id}
+          id={product.id}
+          imagen={product.imagen} 
+          name={product.name} 
+          description={product.description}
+          location={product.location}/>))}
+
+        </Grid>
         :'nada'
 
       }
-
-
-
-            {/* <img src={image} alt='img not found'/>
-            <h3>{id}</h3>
-            <h3>Product: {name}</h3>
-            <h3>Description: {description}</h3>
-            <h3>price: {price}</h3>
-            <h3>Location: {location}</h3> */}
-
-        
-
-
-
-      {/* {boxDetail ?
-      <div>
-        <img src={boxDetail.image} alt='img not found'/>
-        <h4>Ranking: {boxDetail.ranking}</h4>
-        <h2>Box:{boxDetail.name}</h2>
-        <p>Detail:{boxDetail.detail}</p>
-        <h4>Person: {boxDetail.person}</h4>
-        <h4>Price: {boxDetail.price}</h4>
-        <h4>Expiration date: {boxDetail.expiration_date}</h4>
-        {console.log('infor'+boxDetail.product)}
-        
-
-
-
-        
-      </div>
-      :<h3>Loading</h3>
-        } */}
     </div>
+    </Container>
   )
-  {/* {boxDetail.product.map((prod, index) => {
-    console.log(prod)
-  })} */}
-  {/* {boxDetail.product && boxDetail.product.map((product)=> 
-        (<ProductCard
-        name={product.name} 
-        description={product.description}
-        location={product.location}/>))} */}
 }
 
