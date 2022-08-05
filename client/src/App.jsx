39bccf36afd2d48bd35e3057c0e1963e7b573468
Tamaraber9 <tamaraber9@gmail.com>
2022-08-05 17:31:16 -0300

    chanage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Routes>
 
             <Route path="/home" element={<Home />} />
-            <Route path="/detail" element={<BoxDetail />} />
+            {/* <Route path="/detail" element={<BoxDetail />} /> */}
             <Route path="/form" element={<Form />} />
             <Route path="/box/:idBox" element={<BoxDetail />} />
 
