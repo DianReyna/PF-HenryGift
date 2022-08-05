06f14013ab6f3224748a947b83dae20914d557ec
Tamaraber9 <tamaraber9@gmail.com>
2022-08-05 17:02:01 -0300

    change

--- api/src/routes/Boxes.routes.js
+++ api/src/routes/Boxes.routes.js
@@ -5,21 +5,11 @@ const {
   getBox,
   getAllBoxes,
 } = require("../controllers/boxControllers");
-<<<<<<< HEAD
 
 router.get("/", getAllBoxes);
 
 router.get("/:id", getBox);
-=======
->>>>>>> acf1127371563015ff95998f9fecd1e0b65469ba
 
-router.get("/", getAllBoxes);
-
-<<<<<<< HEAD
-=======
-router.get("/:id", getBox);
-
->>>>>>> acf1127371563015ff95998f9fecd1e0b65469ba
 router.post("/", createNewBox);
 
 module.exports = router;
