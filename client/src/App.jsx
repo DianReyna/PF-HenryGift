1bbb83b8e95d8e1f099ac0db138701974d620710
Tamaraber9 <tamaraber9@gmail.com>
2022-08-06 17:27:50 -0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,13 @@ import Home from "./components/Home/Home";
 import BoxDetail from "./components/BoxDetail/BoxDetail";
 import { Route, Routes } from "react-router-dom";
 import { Footer } from "./components/Footer/Footer";
-<<<<<<< HEAD
-import Form from './components/Form/Form'
-
-=======
 import Form from "./components/Form/Form";
->>>>>>> 65feaa97d88c392a5094fd0ef0f8bd730296d4bf
 
-function App() {
+
+function App() 
   return (
     <div className="App">
-<<<<<<< HEAD
+
         <Routes>
             <Route path="/home" element={<Home />} />
             <Route path="/form" element={<Form />} />
@@ -23,18 +19,5 @@ function App() {
 
       </div>
   )
-=======
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        {/* <Route path="/detail" element={<BoxDetail />} /> */}
-        <Route path="/form" element={<Form />} />
-        <Route path="/box/:idBox" element={<BoxDetail />} />
-      </Routes>
-      {/* <Footer /> */}
-    </div>
-  );
-
->>>>>>> 65feaa97d88c392a5094fd0ef0f8bd730296d4bf
-}
 
 export default App;
