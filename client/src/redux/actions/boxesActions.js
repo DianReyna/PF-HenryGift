476708282d6f3864466c26896a035ea65df57fb4
DianReyna <reynaarmas@gmail.com>
2022-08-06 17:42:02 -0500

    delete in boxesAction one line

--- client/src/redux/actions/boxesActions.js
+++ client/src/redux/actions/boxesActions.js
@@ -1,41 +1,39 @@
-import axios from 'axios';
-import { getAllBoxes, getDetailBox } from '../reducer/boxesSlice';
+import axios from "axios";
+import { getAllBoxes, getDetailBox } from "../reducer/boxesSlice";
 
-export const getBoxes=()=>(dispatch)=>{
-    axios('http://localhost:3001/boxes')
-    .then(res=>dispatch(getAllBoxes(res.data)))
-    .catch (err=>console.log(err))
-}
+export const getBoxes = () => (dispatch) => {
+  axios("http://localhost:3001/boxes")
+    .then((res) => dispatch(getAllBoxes(res.data)))
+    .catch((err) => console.log(err));
+};
 
-export const getBoxesPerPage=(pag)=>(dispatch)=>{
-    axios(`http://localhost:3001/boxes?offset=${pag}&limit=4`)
-    .then(res=>dispatch(getAllBoxes(res.data)))
-    .catch (err=>console.log(err))
-}
+export const getBoxesPerPage = (pag) => (dispatch) => {
+  axios(`http://localhost:3001/boxes?offset=${pag}&limit=4`)
+    .then((res) => dispatch(getAllBoxes(res.data)))
+    .catch((err) => console.log(err));
+};
 
-export const searchBox=(name)=>(dispatch)=>{
-    axios('http://localhost:3001/boxes?name=' + name)
-    .then(res=>dispatch(getAllBoxes(res.data)))
-    .catch (err=>console.log(err))
-}
+export const searchBox = (name) => (dispatch) => {
+  axios("http://localhost:3001/boxes?name=" + name)
+    .then((res) => dispatch(getAllBoxes(res.data)))
+    .catch((err) => console.log(err));
+};
 
-export const createProvider=(payload)=>()=>{
-    axios.post('http://localhost:3001/providers', payload)
-}
+export const createProvider = (payload) => () => {
+  axios.post("http://localhost:3001/providers", payload);
+};
 
-export const createBox=(payload)=>()=>{
-
-    var json = axios.post('http://localhost:3001/boxes', payload)
-    return json
-}
+export const createBox = (payload) => () => {
+  var json = axios.post("http://localhost:3001/boxes", payload);
+  return json;
+};
 
 export const detailBox = (id) => (dispatch) => {
-    axios(`http://localhost:3001/boxes/${id}`)
-    .then(res => dispatch(getDetailBox(res.data)))
-    .catch(err => console.log(err))
-    axios.post('http://localhost:3001/boxes', payload)
-}
+  axios(`http://localhost:3001/boxes/${id}`)
+    .then((res) => dispatch(getDetailBox(res.data)))
+    .catch((err) => console.log(err));
+};
 
-export const createProduct=(payload)=>()=>{
-    axios.post('http://localhost:3001/products', payload)
-}
+export const createProduct = (payload) => () => {
+  axios.post("http://localhost:3001/products", payload);
+};
