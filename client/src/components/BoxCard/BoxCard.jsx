30a89948ab9dbd6f43f65eeef9b93448e22f5d28
josetucu92 <josemanuel_seguro2@hotmail.com>
2022-08-06 18:20:33 -0300

    feat/client styles Box and Boxes card styles added

--- client/src/components/BoxCard/BoxCard.jsx
+++ client/src/components/BoxCard/BoxCard.jsx
@@ -1,31 +1,47 @@
 import React from "react";
-import {Box} from '@mui/material'
-import { Stack } from '@mui/material'
-import { Grid } from '@mui/material';
-import {Link} from 'react-router-dom';
-import {Card, CardMedia} from '@mui/material';
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Card, CardMedia } from "@mui/material";
+import {
+  BoxCardTitle,
+  BoxCardOthers,
+  BoxCardDetail,
+  Altura,
+} from "./BoxCardStyles";
 
-export default function BoxCard ({id, name, price, category, ranking, person, expiration_date, detail, image, gift}){
-
-    return(
-
-        <Card sx={{maxWidth:345}}>
-            <CardMedia 
-            component="img"
-            height="140"
-            image={image}
-            alt='img not found'
-            />      
-            <Link to={`/box/${id}`}>{name}</Link>
-            <h3>Ranking: {ranking}</h3>
-            <h3>{name}</h3>
-            <h3>Detail:{detail}</h3>
-            <h3>Persons:{person}</h3>
-            <h3>price: ${price}</h3>
-            <h3>Expiration date: {expiration_date}</h3>
-            {/* <h3>Category: {category}</h3>
-            <h3>Gift:{gift}</h3> */} 
-        </Card>
-
-    );
+export default function BoxCard({
+  id,
+  name,
+  price,
+  ranking,
+  person,
+  expiration_date,
+  detail,
+  image,
+}) {
+  return (
+    <div className="container">
+      <Link to={`/box/${id}`}>
+        <Box>
+          <Card sx={{ maxWidth: 345 }}>
+						
+            <Altura>
+              <CardMedia
+                component="img"
+                height="140"
+                image={image}
+                alt="img not found"
+              />
+              <BoxCardTitle>{name}</BoxCardTitle>
+              <BoxCardOthers>Ranking: {ranking}</BoxCardOthers>
+              <BoxCardDetail>{detail}</BoxCardDetail>
+              <BoxCardOthers>Persons: {person}</BoxCardOthers>
+              <BoxCardOthers>Price: ${price}</BoxCardOthers>
+              <BoxCardOthers>Expiration date: {expiration_date}</BoxCardOthers>
+            </Altura>
+          </Card>
+        </Box>
+      </Link>
+    </div>
+  );
 }
