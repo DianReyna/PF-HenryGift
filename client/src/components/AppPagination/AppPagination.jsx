9391623b58fbbb078358b96013a054c3e71b8371
Alferavil88 <alferavil88@gmail.com>
2022-08-07 14:20:11 -0500

    fixed/api-client searchbar pagination all boxes

--- client/src/components/AppPagination/AppPagination.jsx
+++ client/src/components/AppPagination/AppPagination.jsx
@@ -5,8 +5,8 @@ export default function AppPagination({setPage,page}) {
       return (
         <Box justifyContent={"center"} alignItems="center" display={"flex"} 
         sx={{margin:"20px 0px"}}>
-         <Typography>Page: {page}</Typography>
-         <Pagination count={8} variant="outlined" color="primary" shape="rounded" onChange={(e,value)=>{setPage(value);window.scroll(0,0)}}/>
+         <Typography>Page: {page+1}</Typography>
+         <Pagination count={8} variant="outlined" color="primary" shape="rounded" onChange={(e,value)=>{setPage(value-1);window.scroll(0,0)}}/>
         </Box>
       )
     }
