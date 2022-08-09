553abc1629024913b6297bf66e0f4c1f52cd1126
Alferavil88 <alferavil88@gmail.com>
2022-08-09 16:06:59 -0500

    fix/client pagination boxes home

--- client/src/components/AppPagination/AppPagination.jsx
+++ client/src/components/AppPagination/AppPagination.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import { Pagination, Box, Typography } from "@mui/material";
-
+import { useSelector,useDispatch } from "react-redux";
+import { queryPage } from "../../redux/actions/queryActions";
 
 export default function AppPagination({setPage,page}) {
+
+      const{ boxes} = useSelector((state) => state.boxes)
+      const [pages, setPages] = useState(0)
+      const dispatch = useDispatch()
+
+      useEffect(()=>{
+        
+          setPages(Math.ceil(boxes.count/4))
+          dispatch(queryPage(0))
+         console.log(pages)
+      },[boxes.count])
+
+      useEffect(()=>{
+        setPage(0)
+      },[pages])
+
       return (
         <Box justifyContent={"center"} alignItems="center" display={"flex"} 
         sx={{margin:"20px 0px"}}>
 
         {/*  <Pagination count={8} variant="outlined" color="primary" shape="rounded" onChange={(e,value)=>{setPage(value);window.scroll(0,0)}}/> */}
          <Typography>Page: {page+1}</Typography>
-         <Pagination count={3} variant="outlined" color="primary" shape="rounded" onChange={(e,value)=>{setPage(value-1);window.scroll(0,0)}}/>
+         <Pagination count={pages} page={page+1} variant="outlined" color="primary" shape="rounded" onChange={(e,value)=>{setPage(value-1);window.scroll(0,0)}}/>
 
         </Box>
       )
