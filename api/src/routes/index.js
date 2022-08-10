7e966756e4a16b3164415d0ec9cf04a50259cfdd
Alferavil88 <alferavil88@gmail.com>
2022-08-10 14:05:54 -0500

    feat/api payment route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,7 +8,7 @@ const filtersRoutes = require("./filtersRoutes.js");
 const providerRouter = require("./providerRoutes")
 const sortRouter = require("./sortRoutes")
 const testRouter = require("./testRoutes")
-
+const payment = require("./stripe")
 router.use("/boxes", AllBoxes);
 router.use("/products", products);
 router.use("/categories",categotyRouter)
@@ -18,4 +18,5 @@ router.use("/filters", filtersRoutes);
 router.use("/sort",sortRouter)
 router.use("/test",testRouter)
 router.use("/users",AllUsers)
+router.use("/payment",payment)
 module.exports = router;
