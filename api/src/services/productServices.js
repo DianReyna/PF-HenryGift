f1bcbe15650049eeed454283315e467b60569279
DianReyna <reynaarmas@gmail.com>
2022-08-10 15:44:22 -0500

    add /api delete and update products route

--- api/src/services/productServices.js
+++ api/src/services/productServices.js
@@ -25,16 +25,60 @@ const getProductById = async (id) => {
 };
 
 const getAllProducts = async () => {
-  const allProducts = await Products.findAll(
-    {
-      include: [Provider],
-    }
-  )
-  return allProducts
-}
+  const allProducts = await Products.findAll({
+    include: [Provider],
+  });
+  return allProducts;
+};
 
+const deleteProduct = async (id) => {
+  const destroy = await Products.destroy({
+    where: {
+      id: id,
+    },
+  });
+  return destroy;
+};
+
+const findProvider = async (provider) => {
+  const find = await Provider.findOne({
+    where: {
+      name: provider,
+    },
+  });
+  return find;
+};
+
+const updateProduct = async (id, body) => {
+  const find = await getProductById(id);
+
+  const provi = await findProvider(body.provider);
+
+  if (find.dataValues.provider_id !== provi.dataValues.id) {
+    const data = {
+      ...body,
+      provider_id: provi.dataValues.id,
+    };
+    const updateProvider = await Products.update(data, {
+      where: {
+        id: id,
+      },
+    });
+    return updateProvider;
+  }
+
+  const update = await Products.update(body, {
+    where: {
+      id: id,
+    },
+  });
+  return update;
+};
 module.exports = {
   createNewProduct,
   getProductById,
-  getAllProducts
+  getAllProducts,
+  deleteProduct,
+  findProvider,
+  updateProduct,
 };
