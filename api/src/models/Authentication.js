d0e081e8ce381a6086877ce900f17999ebdf05a9
patriciobaez <baezbuchananpatricio@gmail.com>
2022-08-10 16:13:45 -0300

    models

--- api/src/models/Authentication.js
+++ api/src/models/Authentication.js
@@ -1,18 +1,18 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = BoxFactory = (sequelize) => {
-  return sequelize.define("Gift", {
+  return sequelize.define("Authentication", {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
       primaryKey: true,
     },
-    code: {
+    email: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-    email: {
+    password: {
       type: DataTypes.STRING,
       allowNull: false,
     }
