70dbe5c5f159f1b735890fd55ae22a2c17dc2d1d
DianReyna <reynaarmas@gmail.com>
2022-08-10 23:36:51 -0500

    C:/Program Files/Git/client change in store

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -7,7 +7,6 @@ import products from "../reducer/productsSlice";
 import cartReducer from "../reducer/cartSlice";
 import query from "../reducer/querySlice";
 import users from "../reducer/userSlice";
-import productDelete from "../reducer/productsSlice";
 
 export default configureStore({
   reducer: {
@@ -19,6 +18,5 @@ export default configureStore({
     cart: cartReducer,
     query,
     users,
-    productDelete,
   },
 });
