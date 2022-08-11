6305fcaa4257e3ee06f7d2fcab246fbbfb6e4d5b
DianReyna <reynaarmas@gmail.com>
2022-08-11 14:59:20 -0500

    index routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,19 +5,19 @@ const AllUsers = require("../routes/Users.routes.js");
 const categotyRouter = require("./categoryRoutes");
 const products = require("../routes/Products.routes.js");
 const filtersRoutes = require("./filtersRoutes.js");
-const providerRouter = require("./providerRoutes")
-const sortRouter = require("./sortRoutes")
-const testRouter = require("./testRoutes")
-const payment = require("./stripe")
-const orderRouter = require("./ordersRoutes")
+const providerRouter = require("./providerRoutes");
+const sortRouter = require("./sortRoutes");
+const testRouter = require("./testRoutes");
+const payment = require("./stripe");
+// const orderRouter = require("./ordersRoutes")
 router.use("/boxes", AllBoxes);
 router.use("/products", products);
-router.use("/categories",categotyRouter)
-router.use("/providers",providerRouter)
-router.use("/orders",orderRouter)
+router.use("/categories", categotyRouter);
+router.use("/providers", providerRouter);
+// router.use("/orders",orderRouter)
 router.use("/filters", filtersRoutes);
-router.use("/sort",sortRouter)
-router.use("/test",testRouter)
-router.use("/users",AllUsers)
-router.use("/payment",payment)
+router.use("/sort", sortRouter);
+router.use("/test", testRouter);
+router.use("/users", AllUsers);
+router.use("/payment", payment);
 module.exports = router;
