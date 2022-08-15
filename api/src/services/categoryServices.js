70d2646f00c3eb113aa74e413804ccfeafae8d68
Dian Reyna <reynaarmas@gmail.com>
2022-08-15 03:59:51 -0500

    add new query to database for update and delete

--- api/src/services/categoryServices.js
+++ api/src/services/categoryServices.js
@@ -1,20 +1,37 @@
-const {Category} = require("../database/index")
+const { Category } = require("../database/index");
 
-const createNewCategory = async(categoryName) =>{
+const createNewCategory = async (categoryName) => {
+  let newCategory = await Category.create(categoryName);
+  return newCategory;
+};
 
-    let newCategory = Category.create(categoryName)
-    return newCategory
-}
+const getAllCategories = async () => {
+  let allCategories = await Category.findAll({ attributes: ["id", "name"] });
 
-const getAllCategories = async()=>{
+  return allCategories;
+};
 
-  let allCategories = await Category.findAll({attributes: ['id', 'name']})
- 
-  return allCategories
+const updateCategory = async (body, id) => {
+  const categoryUp = await Category.update(body, {
+    where: {
+      id: id,
+    },
+  });
+  return categoryUp;
+};
 
-}
+const deleteCategory = async (id) => {
+  const destroy = await Category.destroy({
+    where: {
+      id: id,
+    },
+  });
+  return destroy;
+};
 
 module.exports = {
   createNewCategory,
-  getAllCategories
-}
+  getAllCategories,
+  updateCategory,
+  deleteCategory,
+};
