83e2b7dc5ecac9282607df8f3a0e98bb0897fc31
Dian Reyna <reynaarmas@gmail.com>
2022-08-15 03:59:20 -0500

    add new change in provider services

--- api/src/services/providerServices.js
+++ api/src/services/providerServices.js
@@ -1,10 +1,9 @@
 const { Provider } = require("../database/index");
 
 const getAllProviders = async () => {
-  let allProviders = await Provider.findAll();
-  //   {
-  //   attributes: ["id", "name", "phone", "address", "email", "active"],
-  // }
+  let allProviders = await Provider.findAll({
+    attributes: ["id", "name", "phone", "address", "email", "active"],
+  });
   return allProviders;
 };
 
