1b810f74fe602a89dbf50e3e3943711db0a8a040
josetucu92 <josemanuel_seguro2@hotmail.com>
2022-08-16 12:51:06 -0300

    styles added Pagination component

--- client/src/components/AppPagination/AppPagination.jsx
+++ client/src/components/AppPagination/AppPagination.jsx
@@ -1,40 +1,52 @@
-import React, { useEffect,useState } from "react";
-import { Pagination, Box, Typography } from "@mui/material";
-import { useSelector,useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { Pagination, Box } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
 import { queryPage } from "../../redux/actions/queryActions";
 
-export default function AppPagination({setPage,page}) {
-
-      const{ boxes} = useSelector((state) => state.boxes)
-      const [pages, setPages] = useState(0)
-      const dispatch = useDispatch()
-      const query = useSelector((state) => state.query)
-      useEffect(()=>{
-        
-          if(typeof boxes.count != 'undefined'){
-            setPages(Math.ceil(boxes.count/4))
-          }
-          dispatch(queryPage(0))
-      },[boxes.count])
-
-      useEffect(()=>{
-       
-        setPage(0)
-      },[pages])
-
-      useEffect(()=>{
-        if(query.sort.col && query.sort.dir)
-            setPage(0)
-      },[query.sort])
-
-      return (
-        <Box justifyContent={"center"} alignItems="center" display={"flex"} 
-        sx={{margin:"20px 0px"}}>
-
-        {/*  <Pagination count={8} variant="outlined" color="primary" shape="rounded" onChange={(e,value)=>{setPage(value);window.scroll(0,0)}}/> */}
-        {/* <Typography>Page: {page+1}</Typography> */}
-        <Pagination count={pages} page={page+1} variant="outlined" color="primary" shape="rounded" onChange={(e,value)=>{setPage(value-1);window.scroll(0,0)}}/>
-
-        </Box>
-      )
+export default function AppPagination({ setPage, page }) {
+  const { boxes } = useSelector((state) => state.boxes);
+  const [pages, setPages] = useState(0);
+  const dispatch = useDispatch();
+  const query = useSelector((state) => state.query);
+  useEffect(() => {
+    if (typeof boxes.count != "undefined") {
+      setPages(Math.ceil(boxes.count / 4));
     }
+    dispatch(queryPage(0));
+  }, [boxes.count]);
+
+  useEffect(() => {
+    setPage(0);
+  }, [pages]);
+
+  useEffect(() => {
+    if (query.sort.col && query.sort.dir) setPage(0);
+  }, [query.sort]);
+
+  return (
+    <Box
+      justifyContent={"center"}
+      alignItems="center"
+      display={"flex"}
+      sx={{ margin: "2rem 0px" }}
+    >
+      <Pagination
+        sx={{
+          color: "white",
+          button: {
+            color: "white",
+            border: "1px solid white",
+          },
+        }}
+        count={pages}
+        page={page + 1}
+        variant="outlined"
+        shape="rounded"
+        onChange={(e, value) => {
+          setPage(value - 1);
+          window.scroll(0, 0);
+        }}
+      />
+    </Box>
+  );
+}
