69ed5c05891f914bc63f72c208429103357a0cbc
Dian Reyna <reynaarmas@gmail.com>
2022-08-18 13:36:14 -0500

    fix cors and instal

--- api/src/middlewares/setHeaders.js
+++ api/src/middlewares/setHeaders.js
@@ -1,14 +1,14 @@
-const { CORS_URL } = process.env;
+// const { CORS_URL } = process.env;
 
-function setHeaders(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://henry-gift.vercel.app/"); // update * when deploy
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-}
+// function setHeaders(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://henry-gift.vercel.app/"); // update * when deploy
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// }
 
-module.exports = setHeaders;
+// module.exports = setHeaders;
