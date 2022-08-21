58eabf3ca7ea0e5472af397db8de199b0bb7684b
josetucu92 <josemanuel_seguro2@hotmail.com>
2022-08-21 18:57:06 -0300

    refactor stripe check out

---
+++ client/src/components/PayButton/PayButton.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+const PayButton = ({ cartItems }) => {
+  const { user } = useSelector((state) => state.auth);
+
+  const handleCheckOut = () => {
+    axios
+      .post(`http://localhost:3001/stripe/create-checkout-session`, {
+        cartItems,
+        userId: user._id,
+      })
+      .then((res) => {
+        if (res.data.url) {
+          window.location.href = res.data.url;
+        }
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleCheckOut()}>Check Out</button>
+    </div>
+  );
+};
+
+export default PayButton;
