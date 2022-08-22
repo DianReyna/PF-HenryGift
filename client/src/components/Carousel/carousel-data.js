d78173b233427b037b3e886499a37e5f44c0c8d4
DaybelCR <dyi.cisnerosr@gmail.com>
2022-08-22 11:13:14 -0500

    added letters in carousel component

---
+++ client/src/components/Carousel/carousel-data.js
@@ -0,0 +1,30 @@
+export const carouselData=[
+    {
+      image:"https://www.zadar.hr/datastore/imagestore/1920x900/1920x900_1563031946adventure_and_outdoor.jpg?v=1563032988",
+      title: "Give amazing surprises",
+      description:"Family moments"
+   },
+    {
+        image: "https://www.hola.com/imagenes/viajes/2012101961330/patagonia-argentina-america/0-218-9/a_Escalada-en-hielo,-Parqu-a.jpg",
+        title: "Give incredible experiences",
+        description: "Unique moments of happiness"
+    },
+    {
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/18/c5/07/40/tabla-de-quesos-mixtos.jpg",
+        title: "Surprise with new flavors",
+        description: "Gifts to share"
+    },
+    {
+        image: "https://www.revistauncamino.com.ar/wp-content/uploads/2022/05/termas-rio-hobdo-1.jpg",
+        title: "Give incredible experiences",
+        description: "Unique moments of happiness"
+    },{
+        image :"https://cache.marriott.com/is/image/marriotts7prod/si-limsi-mariva-restaurant--30991:Feature-Hor",
+        title: "Give and share",
+        description :"A delight to your sense of taste"
+    },{
+        image:"https://media-cdn.tripadvisor.com/media/photo-s/0d/4e/51/0a/room-for-individualtreatments.jpg",
+        title :"Surprise yourself",
+        description:"Moments of relaxation"
+    }
+  ]
