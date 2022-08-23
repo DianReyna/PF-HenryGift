0274567e203bba5510ac1e8c36411ac7fc4d9b90
Tamaraber9 <tamaraber9@gmail.com>
2022-08-23 01:34:13 -0300

    histori

--- client/src/components/User/OrderHistory.jsx
+++ client/src/components/User/OrderHistory.jsx
@@ -11,44 +11,26 @@ export default function OrderHistory() {
 
     const {user}  = useSelector((state) => state.auth);
     const {orderDetail} = useSelector((state)=> state.users);
-        
+          
     useEffect(()=>{
         dispatch(getOrders(user._id))
         },[dispatch])
-    
-         
+
+            
       const columns = [
         { field: 'id', headerName: 'ID', width: 150 },
-        { field: 'quantity', headerName: ' Quantity', width: 150 },
-        { field: 'is_gift', headerName: 'Gift', width: 150 },
-        { field: 'recipient', headerName: 'Recipient', width: 150 },
-        { field: 'amount', headerName: 'Amount', width: 150 },
-        { field: 'name', headerName: 'Name', width: 150 },
-        { field: 'price', headerName: 'Price', width: 150 },
-        { field: 'ranking', headerName: 'Ranking', width: 150 },
-        { field: 'expiration_date', headerName: 'Expiration Date', width: 150 },
-        { field: 'person', headerName: 'Person', width: 150 },
-        { field: 'active', headerName: 'Active', width: 150 },
+        { field: 'amount', headerName: 'Amount (UDS)', width: 150 },
        ];
 
        const rows = orderDetail.map((row)=>({
         id: row.id,
-        quantity: row.quantity,
-        is_gift: row.is_gift,
-        recipient: row.recipient,
-        amount: row.amount,
-        name: row.name,
-        price: row.price,
-        ranking: row.ranking,
-        expiration_date: row.expiration_date,
-        person: row.person,
-        active: row.active,
-        
+        amount: row.amount,       
        }))
 
       return (
-      <Box sx={{ height: 400, width: '100%' }}>
+      <Box sx={{ height: 400, width: '50%' }}>
       <DataGrid
+        style={{ color: "white", textAlign:'center' }}
         rows={rows}
         columns={columns}
         pageSize={5}
@@ -60,4 +42,5 @@ export default function OrderHistory() {
     </Box>
       );
  }
+
 
