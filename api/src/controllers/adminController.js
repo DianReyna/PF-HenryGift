3a78a4098eae9bd345a555f1a33585e71aa07381
Dian Reyna <reynaarmas@gmail.com>
2022-08-24 03:39:01 -0500

    change code in controller admin and added count amount

--- api/src/controllers/adminController.js
+++ api/src/controllers/adminController.js
@@ -8,83 +8,41 @@ const {
   User,
   Favourite,
   Order,
+  GiftList,
 } = require("../database/index");
 
-const salesBox = async (req, res, next) => {
-  const order = await OrderDetail.findAll({
-    include: [
-      {
-        model: Box,
-      },
-    ],
-    include: [
-      {
-        model: Order,
-        attributes: ["amount"],
-        include: [
-          {
-            model: User,
-            attributes: ["first_name", "last_name", "banned"],
-          },
-        ],
-      },
-    ],
-  });
+const adminServices = require("../services/adminServices");
 
-  res.send(order);
+const salesBox = async (req, res, next) => {
+  try {
+    const order = await adminServices.salesBox();
+    res.status(200).send(order);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const raitingBox = async (req, res, next) => {
-  const ratingBox = await Box.findAll({
-    order: [["ranking", "DESC"]],
-    where: {
-      ranking: {
-        [Op.gte]: 4.5,
-      },
-    },
-  });
-  res.send(ratingBox);
-};
-
-const favouriteBox = async (req, res, next) => {
-  const favourites = await Favourite.findAll({
-    group: "box_id",
-  });
-
-  res.send(favourites);
+  try {
+    const raiting = await adminServices.raitingBox();
+    res.status(200).send(raiting);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const userSales = async (req, res, next) => {
-  const amount = await OrderDetail.count({
-    group: ["UserEmail"],
-    order: [["UserEmail", "DESC"]],
-  });
+  try {
+    const user = await adminServices.userSales();
 
-  const user = await Order.findAll({
-    include: [
-      {
-        model: User,
-        attributes: ["first_name", "last_name", "banned"],
-      },
-    ],
-  });
-
-  const sale = await Order.findAll({
-    attributes: ["UserEmail", [fn("sum", col("amount")), "total"]],
-    group: ["UserEmail"],
-    raw: true,
-  });
-  const newRes = {
-    orderDetail: amount,
-    order: user,
-    sale: sale,
-  };
-  res.send(newRes);
+    res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   salesBox,
   raitingBox,
-  favouriteBox,
   userSales,
 };
