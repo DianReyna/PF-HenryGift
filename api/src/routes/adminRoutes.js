f251db66210113e96aa1f290120d7b56dd946c2f
Dian Reyna <reynaarmas@gmail.com>
2022-08-24 03:42:43 -0500

    change routes admin delete route favourite in admin

--- api/src/routes/adminRoutes.js
+++ api/src/routes/adminRoutes.js
@@ -3,12 +3,10 @@ const router = Router();
 const {
   salesBox,
   raitingBox,
-  favouriteBox,
   userSales,
 } = require("../controllers/adminController");
 
 router.get("/raiting", raitingBox);
-router.get("/favourites", favouriteBox);
 router.get("/sales", userSales);
 router.get("/", salesBox);
 
