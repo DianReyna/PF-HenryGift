140f607dee8f0dd0503abc630cc5e6e204b5d969
Dian Reyna <reynaarmas@gmail.com>
2022-08-24 21:20:06 -0500

    change reviewActions

--- client/src/redux/actions/reviewsActions.js
+++ client/src/redux/actions/reviewsActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getView, postViews } from "../reducer/reviewsSlice";
 
-// const URL = " https://henrygift-api.herokuapp.com";
+// const URL=" https://henrygift-api.herokuapp.com/register"
 const URL = "http://localhost:3001";
 
 export const getReviews = (id) => (dispatch) => {
@@ -17,5 +17,11 @@ export const createReviews = (id, scoreBox, menssageBox) => (dispatch) => {
   axios
     .post(`${URL}/reviews?user_id=${id.user}&box_id=${id.box}`, data)
     .then((res) => dispatch(postViews(res.data)))
+    .catch((err) => console.log(err));
+};
+
+export const getUserReviews = (box_id, user_id) => (dispatch) => {
+  axios(` ${URL}/reviews/userReview?box_id=${box_id}&user_id=${user_id}`)
+    .then((res) => dispatch(getView(res.data)))
     .catch((err) => console.log(err));
 };
