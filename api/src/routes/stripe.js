b609f3222d8afe1e6e4da2364080620f339f943c
Dian Reyna <reynaarmas@gmail.com>
2022-08-25 09:44:34 -0500

    change url stripe

--- api/src/routes/stripe.js
+++ api/src/routes/stripe.js
@@ -42,6 +42,7 @@ router.post("/create-checkout-session", async (req, res, next) => {
 });
 
 // const URL = `http://127.0.0.1:5173`
-const URL = `https://henry-gift.vercel.app`;
+// const URL = `https://henry-gift.vercel.app`;
+const URL = "https://henrygift-api.herokuapp.com";
 
 module.exports = router;
